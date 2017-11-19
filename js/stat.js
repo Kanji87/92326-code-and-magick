e042e1333d9948cf46274ee8b6d7d17975a4a302
kanj <suslik87@gmail.com>
2017-11-19 16:55:21 +0300

    вывод на канвасе "облака" с надписями

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,12 @@
 'use strict';
-var renderStatistics = function () {
-  console.log();
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, .7)';
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillStyle = '#000';
+  ctx.font = 'PT Mono 18px';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
 };
