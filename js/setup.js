3e331c61d9d524902ff804d5bfb9c0c090a483bb
Ruslan Polonkoev <suslik87@gmail.com>
2017-11-22 12:48:20 +0300

    создал массив объектов с магами

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 document.querySelector('.setup').classList.remove('hidden');
 
-var wizardFirstNames = [
+var firstNames = [
   'Иван',
   'Хуан Себастьян',
   'Мария',
@@ -12,7 +12,7 @@ var wizardFirstNames = [
   'Вашингтон'
 ];
 
-var wizardLastNames = [
+var lastNames = [
   'да Марья',
   'Верон',
   'Мирабелла',
@@ -23,13 +23,36 @@ var wizardLastNames = [
   'Ирвинг'
 ];
 
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var eyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+var getRandomArrIndex = function (arr) {
+  return arr[getRandomInt(0, arr.length - 1)];
+};
+
 var renderName = function (isRandom) {
-  var firstName = wizardFirstNames[getRandomInt(0, wizardFirstNames.length - 1)];
-  var lastName = wizardLastNames[getRandomInt(0, wizardLastNames.length - 1)];
+  // var firstName = firstNames[getRandomInt(0, firstNames.length - 1)];
+  // var lastName = lastNames[getRandomInt(0, lastNames.length - 1)];
+  var firstName = getRandomArrIndex(firstNames);
+  var lastName = getRandomArrIndex(lastNames);
   var name = firstName + ' ' + lastName;
   if (isRandom === true) {
     if (getRandomInt(0, 1)) {
@@ -41,6 +64,23 @@ var renderName = function (isRandom) {
 
 var wizards = [
   {
-    name: renderName()
+    name: renderName(),
+    coatColor: getRandomArrIndex(coatColors),
+    eyesColor: getRandomArrIndex(eyesColors)
+  },
+  {
+    name: renderName(),
+    coatColor: getRandomArrIndex(coatColors),
+    eyesColor: getRandomArrIndex(eyesColors)
+  },
+  {
+    name: renderName(),
+    coatColor: getRandomArrIndex(coatColors),
+    eyesColor: getRandomArrIndex(eyesColors)
+  },
+  {
+    name: renderName(),
+    coatColor: getRandomArrIndex(coatColors),
+    eyesColor: getRandomArrIndex(eyesColors)
   }
 ];
